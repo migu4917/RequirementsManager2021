ddd5040aab0768c6c51f4f40169c523e00516601
migu4917 <491745115@qq.com>
2021-02-24 17:52:52 +0800

    add docx parser and outline tree structure

---
+++ rm-server/templatemanager/templatemanager/dao/template_outline.py
@@ -0,0 +1,21 @@
+import json
+
+
+class OutlineNode:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def addChild(self):
+        pass
+
+
+class OutlineTree:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    print(json.dumps({"test": "test"}))
