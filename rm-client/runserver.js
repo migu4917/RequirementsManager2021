9af1cb153d29939530d818a96aba33bf33194eec
migu4917 <491745115@qq.com>
2021-02-24 17:52:01 +0800

    delete runserver

--- rm-client/runserver.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-app.use(express.static('./dist'))
-
-app.listen(80, () => {
-    console.log('web server running at http://127.0.0.1')
-})
